5a4b2e489173b9ffe3f8c3298e4f7333551b1873
Sandrina Pereira <sandrin4pereir4@gmail.com>
2017-04-29 15:28:00 +0100

    develop border, padding, margin and position converters

--- index.js
+++ index.js
@@ -1,16 +1,44 @@
 var postcss = require('postcss');
 
-/* eslint-disable */
-module.exports = postcss.plugin('postcss-start-to-end', function (opts) {
+/* eslint indent: ["error", 4, { "SwitchCase": 1 }] */
+module.exports = postcss.plugin('postcss-start-to-end', opts => {
+
+    const defaultOpts = {
+        direction: 'LTR',
+        warnings: 'true'
+    };
     opts = opts || {};
 
-    // Work with options here
+    const finalOpts = Object.assign({}, defaultOpts, opts);
 
-    return function (root, result) {
+    const logic = {
+        start: finalOpts.direction === 'LTR' ? 'left' : 'right',
+        end: finalOpts.direction === 'LTR' ? 'right' : 'left'
+    };
 
-        // some code
+    return (root) => {
 
+        root.walkDecls(rule => {
+            switch (rule.prop) {
+                case 'start':
+                case 'border-start':
+                case 'border-top-start':
+                case 'border-bottom-start':
+                case 'padding-start':
+                case 'margin-start':
+                    rule.prop = rule.prop.replace('start', `${logic.start}`);
+                    break;
+
+                case 'end':
+                case 'border-end':
+                case 'border-top-end':
+                case 'border-bottom-end':
+                case 'padding-end':
+                case 'margin-end':
+                    rule.prop = rule.prop.replace('end', `${logic.end}`);
+                    break;
+                default:
+            }
+        });
     };
 });
-
-/* eslint-enable */
--- index.test.js
+++ index.test.js
@@ -10,6 +10,134 @@ function run(input, output, opts) {
         });
 }
 
-it('does something', () => {
-    return run('a{ }', 'a{ }', { });
+var css = '';
+var cssOutputted = '';
+
+/* ==================================================================
+    Border Tests
+================================================================== */
+
+it('LTR: border-start -> border-left', () => {
+    css = '.foo { border-start: 1px solid teal; }';
+    cssOutputted = '.foo { border-left: 1px solid teal; }';
+
+    return run(css, cssOutputted, { });
+});
+
+it('LTR: border-end -> border-right', () => {
+    css = '.foo { border-end: 1px solid teal; }';
+    cssOutputted = '.foo { border-right: 1px solid teal; }';
+
+    return run(css, cssOutputted, { });
+});
+
+it('RTL: border-start -> border-right', () => {
+    css = '.foo { border-start: 1px solid teal; }';
+    cssOutputted = '.foo { border-right: 1px solid teal; }';
+
+    return run(css, cssOutputted, { direction: 'RTL' });
+});
+
+it('RTL: border-end -> border-left', () => {
+    css = '.foo { border-end: 1px solid teal; }';
+    cssOutputted = '.foo { border-left: 1px solid teal; }';
+
+    return run(css, cssOutputted, { direction: 'RTL' });
+});
+
+/* ==================================================================
+    Padding Tests
+================================================================== */
+
+it('LTR: padding-start -> padding-left', () => {
+    css = '.foo { padding-start: 1rem; }';
+    cssOutputted = '.foo { padding-left: 1rem; }';
+
+    return run(css, cssOutputted, { });
+});
+
+it('LTR: padding-end -> padding-right', () => {
+    css = '.foo { padding-end: 1rem; }';
+    cssOutputted = '.foo { padding-right: 1rem; }';
+
+    return run(css, cssOutputted, { });
+});
+
+it('RTL: padding-start -> padding-right', () => {
+    css = '.foo { padding-start: 1rem; }';
+    cssOutputted = '.foo { padding-right: 1rem; }';
+
+    return run(css, cssOutputted, { direction: 'RTL' });
+});
+
+it('RTL: padding-end -> padding-left', () => {
+    css = '.foo { padding-end: 1rem; }';
+    cssOutputted = '.foo { padding-left: 1rem; }';
+
+    return run(css, cssOutputted, { direction: 'RTL' });
+});
+
+
+/* ==================================================================
+    Margin Tests
+================================================================== */
+
+it('LTR: margin-start -> margin-left', () => {
+    css = '.foo { margin-start: 1rem; }';
+    cssOutputted = '.foo { margin-left: 1rem; }';
+
+    return run(css, cssOutputted, { });
+});
+
+it('LTR: margin-end -> margin-right', () => {
+    css = '.foo { margin-end: 1rem; }';
+    cssOutputted = '.foo { margin-right: 1rem; }';
+
+    return run(css, cssOutputted, { });
+});
+
+it('RTL: margin-start -> margin-right', () => {
+    css = '.foo { margin-start: 1rem; }';
+    cssOutputted = '.foo { margin-right: 1rem; }';
+
+    return run(css, cssOutputted, { direction: 'RTL' });
+});
+
+it('RTL: margin-end -> margin-left', () => {
+    css = '.foo { margin-end: 1rem; }';
+    cssOutputted = '.foo { margin-left: 1rem; }';
+
+    return run(css, cssOutputted, { direction: 'RTL' });
+});
+
+/* ==================================================================
+    Position Tests
+================================================================== */
+
+it('LTR: start -> left', () => {
+    css = '.foo { start: 1rem; }';
+    cssOutputted = '.foo { left: 1rem; }';
+
+    return run(css, cssOutputted, { });
+});
+
+it('LTR: end -> right', () => {
+    css = '.foo { end: 1rem; }';
+    cssOutputted = '.foo { right: 1rem; }';
+
+    return run(css, cssOutputted, { });
+});
+
+it('RTL: start -> right', () => {
+    css = '.foo { start: 1rem; }';
+    cssOutputted = '.foo { right: 1rem; }';
+
+    return run(css, cssOutputted, { direction: 'RTL' });
+});
+
+it('RTL: end -> left', () => {
+    css = '.foo { end: 1rem; }';
+    cssOutputted = '.foo { left: 1rem; }';
+
+    return run(css, cssOutputted, { direction: 'RTL' });
 });
